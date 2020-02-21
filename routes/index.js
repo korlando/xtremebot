5e415a7fd03c5da12692956c3481c5534c21b10b
Keith Orlando <keith@airk.ai>
2020-02-21 03:00:20 -0500

    add /slack/events endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+router.post('/slack/events', (req, res) => {
+  const { challenge } = req.body;
+  res.set('Content-Type', 'text/plain');
+  res.status(200).send(challenge);
+});
+
 module.exports = router;
