2a4f189308c189ca5cb3972d58ea5fbb32cf8149
Keith Orlando <keith@airk.ai>
2020-02-23 03:59:49 -0500

    add another bot instance; check team_id in handleMessageEvent

--- slack/welcomebot/index.js
+++ slack/welcomebot/index.js
@@ -4,16 +4,25 @@ const methods = require('../methods');
 const instances = [
 	{
 		web: new WebClient(process.env.TEST_WELCOMEBOT_SLACK_TOKEN),
+		botTeamId: 'TC6DG4XED',
 		botUserId: 'UU3RUS61J',
+	},
+	{
+		web: new WebClient(process.env.MK_XTREMEBOT_SLACK_TOKEN),
+		botTeamId: 'TRF1B2NN4',
+		botUserId: 'UUF4Y0ZV4',
 	},
 ];
 
 const handleMessageEvent = async (slackEvent) => {
-	const { event } = slackEvent;
+	const { event, teamId } = slackEvent;
 	const { text, channel, user } = event;
 
 	instances.forEach(async (instance) => {
-		const { web, botUserId } = instance;
+		const { web, botUserId, botTeamId } = instance;
+		if (teamId !== botTeamId) {
+			return;
+		}
 		if (user === botUserId) {
 			// prevent cycle where bot responds to itself
 			return;
